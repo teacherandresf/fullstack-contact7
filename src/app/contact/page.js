54d677eaa57036284ac2529f2359d3070f145593
Andres <aferrod0406@g.educaand.es>
2025-03-03 09:54:44 +0100

    solucion

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,40 +1,45 @@
-"use client"
-import {useState, useEffect} from 'react'
-import Link from 'next/link'
-import 'C:/Users/Andrés/Documents/PROYECTOS-DI/fullstack-contact7/src/styles/estilosinicio.css'
+"use client";
+import { useState, useEffect } from "react";
+import Link from "next/link";
 
-export default function ListContact(){
+export default function ListContact() {
+  const [contacts, setContacts] = useState([]);
 
-    const [contacts, setContacts] = useState([])
+  async function deleteContact(deleteId) {
+    if (window.confirm("Desea eliminarlo")) {
+      const response = await fetch("/api/contact", {
+        method: "DELETE",
+        headers: { "Content-Type": "application-json" },
+        body: JSON.stringify({ id: deleteId }),
+      });
 
-    async function deleteContact(deleteId){
-        
-        if(window.confirm("Desea eliminarlo")){
-        const response = await fetch("/api/contact", {
-            method: 'DELETE',
-            headers: {"Content-Type":"application-json"},
-            body: JSON.stringify({id: deleteId})
-        })
-
-        fetchContacts()
-        }
+      fetchContacts();
     }
+  }
 
-    async function fetchContacts(){
-        const response = await fetch("/api/contact")
-        const body = await response.json()
-        setContacts(body)
-    }
+  async function fetchContacts() {
+    const response = await fetch("/api/contact");
+    const body = await response.json();
+    setContacts(body);
+  }
 
-    useEffect( () => {
-        fetchContacts()
-    }, [])
+  useEffect(() => {
+    fetchContacts();
+  }, []);
 
-    return(
+  return (
     <div>
-        <h1>Lista de contactos: </h1>
-        
-        {contacts.map(contact => <p key={contact.id}><Link  href={"/contact/" + contact.id} >{contact.name} {contact.surnames}</Link><button onClick={() => deleteContact(contact.id)}>Eliminar</button></p>)}
-        <Link href="/contact/create" >Agregar contacto</Link>
-    </div>);
+      <h1>Lista de contactos: </h1>
+
+      {contacts.map((contact) => (
+        <p key={contact.id}>
+          <Link href={"/contact/" + contact.id}>
+            {contact.name} {contact.surnames}
+          </Link>
+          <button onClick={() => deleteContact(contact.id)}>Eliminar</button>
+        </p>
+      ))}
+      <Link href="/contact/create">Agregar contacto</Link>
+    </div>
+  );
 }
